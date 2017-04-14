f6dbbcc658e6edf3409bad32b7243bb92365c993
kmhegeman <hegeman.1@osu.edu>
2017-04-13 22:28:26 -0400

    Update FlightStatus.cs

--- Models/FlightStatus.cs
+++ Models/FlightStatus.cs
@@ -1,17 +1,23 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace FisherAirlines.Models
 {
     public class FlightStatus
     {
+        [Key]
+        public int FlightId { get; set; }
 
-        public int Id { get; set; }
-
-        public DateTime FlightDate { get; set; }
-
-        public string Source { get; set; }
+        public string Departure { get; set; }
 
         public string Destination { get; set; }
+
+        public DateTime DepartDate { get; set; }
+
+        [DataType(DataType.Time)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:H:mm}")]
+        public DateTime ArrivalTime { get; set; }
          
         public string Status { get; set; }
 
