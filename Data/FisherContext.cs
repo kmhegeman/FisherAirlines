d3517412d89174d96af2bb8b317df77f69a479c0
kmhegeman <hegeman.1@osu.edu>
2017-04-13 22:36:32 -0400

    Update FisherContext.cs

--- Data/FisherContext.cs
+++ Data/FisherContext.cs
@@ -1,17 +1,42 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using FisherAirlines.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations; 
+using System.Collections.Generic;
+
+
 
 namespace FisherAirlines.Data
 {
     public class FisherContext : IdentityDbContext <ApplicationUser>
     {
+    
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-    string connection = "Data Source=C:/AMIS3610/FisherAirlines/fisher-airlines.sqlite;";
+    string connection = "Data Source=c:/AMIS3610/FisherAirlines/fisher-airlines.sqlite;";
     optionsBuilder.UseSqlite(connection);
     }
-public DbSet<Flight> Flights { get; set; }
-public DbSet<FlightStatus> FlightStatus { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        modelBuilder.Entity<Flight>().HasKey(x => x.Id);
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Bookings>().HasKey(b => b.ConfirmationNumber);
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<FlightStatus>().HasKey(y => y.FlightId);
+        base.OnModelCreating(modelBuilder);
+
+    }
+
+    public DbSet<Flight> Flight { get; set; }
+    public DbSet<FlightStatus> FlightStatus { get; set; }
+    public DbSet<Bookings> Bookings { get; set; }
+
     }
 }
