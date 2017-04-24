483ae382bfe0b013e33651efd48d6e9087b8a73c
Emily Grosjean <Emily Grosjean>
2017-04-24 13:34:12 -0400

    update status component to get url to work properly

--- FisherAirlines-ross/wwwroot/app/components/status/status.component.ts
+++ FisherAirlines-ross/wwwroot/app/components/status/status.component.ts
@@ -3,6 +3,8 @@ import { RouteParams, Router } from '@angular/router';
 import { FlightStatus } from './flightStatus';
 import { FlightService } from '../../flight.service';
 import { AuthService } from '../../auth.service';
+import { FormBuilder, Validators, FormGroup, CustomValidators } from '@angular/forms';
+
 
 @Component({
  selector: 'flight-status',
@@ -11,18 +13,41 @@ import { AuthService } from '../../auth.service';
 })
 
 export class StatusComponent implements OnInIt {
+    FlightStatusForm: FormGroup;
     public flightStatus: FlightStatus[];
+    public selectedFlight: FlightStatus;
+    errorMessage: string;
+    today: Date = new Date();
+    FlightId: number;
+    Destination: string;
+    Departure: string;
+    DepartDate: Date;
 
 
-    constructor(private flightService: FlightService) {
+
+    constructor(private fb: FormBuilder, private flightService: FlightService, private router: Router) 
+    { 
+        this.FlightStatusForm = fb.group({
+            FlightId: [''],
+            Destination: ['', Validators.maxLength(20)],
+            Departure: ['', Validators.maxLength(20)],
+            DepartDate: ['']
+        })
     }
 
-    ngOnInIt(Destination, Departure, DepartDate, FlightId) {
-        this.GetFlights(Destination, Departure, DepartDate, FlightId);
-    }
+    //ngOnInIt(Destination, Departure, DepartDate, FlightId) {
+        //this.GetFlights(Destination, Departure, DepartDate, FlightId);
+    //}
 
-    GetFlightStatus(FlightId, Destination, Departure, DepartDate) {
-        this.flightService.GetFlightStatus(FlightId, Destination, Departure, DepartDate).then(flights => this.flightStatus = flights);
+    GetFlightStatus(FlightStatusForm) 
+    {
+        let FlightId = this.FlightStatusForm.value.FlightId
+        let Destination = this.FlightStatusForm.value.Destination
+        let Departure = this.FlightStatusForm.value.Departure
+        let DepartDate = this.FlightStatusForm.value.DepartDate
+        this.router.navigate(['/flightstatus/GetFlightStatus'], {queryParams: { FlightId, Destination, Departure, DepartDate }});
     }
+       
+}
 
-} 
+ 
